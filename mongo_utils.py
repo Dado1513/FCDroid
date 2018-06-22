b5519ead8e7911843fe7c18658a99c0733d24893
Dado1513 <dave.caputo93@gmail.com>
2018-06-22 14:35:36 +0200

    add pymongo

--- mongo_utils.py
+++ mongo_utils.py
@@ -8,7 +8,7 @@ class MongoDB:
             self.db_url = os.environ['DATABASE_URL']
             self.client = MongoClient(self.db_url, connect=False)
         else:
-            self.db_url = 'mongodb://localhost:27117/db'
+            self.db_url = 'mongodb://localhost:27117/db' # conencto to mongodb inside docker
             self.client = MongoClient(self.db_url, connect=False)
         try:
             self.db = self.client['db']
@@ -21,9 +21,15 @@ class MongoDB:
             search if analysis are  yet executed
         """
         result = self.client.find_one({"name_apk":apk_name})
+        return result
 
     def insert_analysis(self,apk,retire_local,retire_remote):
         """
             function to insert element in mongo db after scan
 
         """
+        dict_to_insert = dict()
+        dict_to_insert["html_file"] = list(apk.html_file.keys()) # all html file
+        dict_to_insert["js_file"] = list(apk.javascript_file.keys())
+        dict_to_insert["is_hybrid"] = str(apk.is_hybrid)
+        dict_to_insert["permission"] = apk.get_permissions()
