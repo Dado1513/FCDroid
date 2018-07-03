122bced8b70c3f108649996b5ded258b9d98fa31
Dado1513 <dave.caputo93@gmail.com>
2018-07-03 16:13:32 +0200

    fix readme

--- MyAPK.py
+++ MyAPK.py
@@ -492,7 +492,7 @@ class MyAPK:
         """
         html_dir = "temp_html_code/html_downloaded_"+self.name_only_apk
         print(bcolors.WARNING+"\n[*] Download remote page in: "+html_dir+bcolors.ENDC)
-        cmd_wget = ['wget' ,'-E', '-H' ,'-k',' -K ','-T','5','-nd' ,'-N', '-p','-P', html_dir]
+        cmd_wget = ['wget' ,'-E', '-H' ,'-k',' -K ','-T','1','-nd' ,'-N', '-p','-P', html_dir]
         FNULL = open(os.devnull, 'w')
         for url in self.url_loaded:
             if url[-1] == "/":
@@ -502,8 +502,14 @@ class MyAPK:
             name_file = url.split("/")[-1]
             path_complete = html_dir+"/"+name_file
             self.name_to_url[path_complete] = url 
-            self.file_download_to_analyze[path_complete] = False
+            # self.file_download_to_analyze[path_complete] = False
             cmd_wget.remove(url)
+
+        # now add all file download in this dir with extension html and js
+        for root, _, files in os.walk(html_dir):
+                for file_download in files:
+                    if file_download.endswith(".html") or file_download.endswith(".js"):
+                        self.file_download_to_analyze[os.path.join(root,file_download)] = False
             
     def download_page_loaded(self):
         """
@@ -626,7 +632,8 @@ class MyAPK:
                 ip = re.findall(r"[0-9]+(?:\.[0-9]+){3}",url)
                 if ip != None and len(ip) > 0:
                     # change ip with host
-                    url_new = url.replace(ip[0],self.network_dict[keys]["host"])
+                    # get only first element of every list --> every list are max 1 element
+                    url_new = url.replace(ip[0],self.network_dict[keys]["host"][0])
                     url_list_new.append(url_new)
                 else:
                     url_list_new.append(url)
--- README.md
+++ README.md
@@ -5,22 +5,25 @@ Simple tool written in python that is able to:
 
 - Look for strings inside passed through CLI
 
-- If you search "iframe" able to identify frame confusion
+- If you search "iframe" able to identify frame_confusion problem
 
-- Necessary to have in PATH the dir of apktool
+- Necessary to have in PATH the dir of apktool 
+
+- Necessary download retirejs[https://retirejs.github.io/retire.js/]
+
 ```
-Usage: pythonAnaconda3.6 hybrid_inspector.py -d "/home/User/dir_apk" -t -s "iframe"
+Usage: python3.6 hybrid_inspector.py -d "/home/User/dir_apk" -t -s "iframe"
 ```
 ___
 Work In Progress:
-- [ ] search method used using smali code invece che androguard
 - [x] Search loadUrl e string used inside
-- [ ] Search loadUrl e string used if assigned to a variable before (done apimonitor)
+- [x] Search loadUrl e string used if assigned to a variable before (done apimonitor)
 - [x] Download web page used 
-- [ ] loadUrl/EvaluateJavaScript --> containe javascript code exec (apimonitor)
 - [x] log output on file using logging
 - [x] controllare enableJavaScript se passa argomento true
 - [x] url runtime (using dynamic analysis before) such as droidbot and get file apimonitor
-- [ ] whitelist url(intent/network/navigation)
-- [ ] check iframe in js script
+- [x] whitelist url(intent/network/navigation)
+- [x] check iframe in js script
+- [x] identify dom xss static
+- [x] use retirejs 
 ___
