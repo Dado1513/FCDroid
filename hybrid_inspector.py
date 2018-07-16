79dfbccdae81a3d40d0cfbda71f7bb93a476f59f
Dado1513 <dave.caputo93@gmail.com>
2018-07-16 22:05:09 +0200

    make analysis faster

--- hybrid_inspector.py
+++ hybrid_inspector.py
@@ -87,11 +87,14 @@ def analyze_start(conf, apk_to_analyze, tag, string_to_find, api_monitor_dict=No
                 apk.vulnerable_frame_confusion()
                 if apk.is_vulnerable_frame_confusion:
                     # TODO se file vulnerable == file_js_with_iframe modificare i controlli
-                    print(bcolors.FAIL + "\nThis app might be vulnerable on attack frame confusion." +bcolors.ENDC)
                     apk_vulnerable.append(apk_to_analyze)
+                    if not apk.file_vulnerable_frame_confusion == apk.file_js_with_iframe:
+                        print(bcolors.FAIL + "\nThis app might be vulnerable on attack frame confusion." +bcolors.ENDC)
+                        print(bcolors.FAIL + "This file are vulnerable " + str(apk.file_vulnerable_frame_confusion)+bcolors.ENDC)
+                        logger.logger.info("This app might be vulnerable on attack frame confusion, This file are vulnerable %s", str(apk.file_vulnerable_frame_confusion))
+                    else:
+                        print(bcolors.WARNING + "\nThis app might be vulnerable on attack frame confusion (found string iframe inside js file)." +bcolors.ENDC)
 
-                    print(bcolors.FAIL + "This file are vulnerable " + str(apk.file_vulnerable_frame_confusion)+bcolors.ENDC)
-                    logger.logger.info("This app might be vulnerable on attack frame confusion, This file are vulnerable %s", str(apk.file_vulnerable_frame_confusion))
                     if len(apk.file_js_with_iframe) > 0:
                         apk_maybe_vulnerable.append(apk_to_analyze)
                         print()
